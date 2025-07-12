fa0999184fa802bd4ca6a9ed40f73213a806a07f
Sumit Saini <sumitsaini1968@gmail.com>
2025-07-12 17:52:45 +0530

    Update songs.js

--- src/data/songs.js
+++ src/data/songs.js
@@ -3,7 +3,7 @@ const songs = [
   { name: 'Supreme', file: 'songs/supreme.mp3', cover: 'covers/cover.jpeg' },
   { name: 'Zinda', file: 'songs/zinda.mp3', cover: 'covers/cover.jpeg' },
   { name: 'Chak Lein De', file: 'songs/chak_lein_de.mp3', cover: 'covers/cover.jpeg' },
-  { name: 'Kar Har Maidaan Fateh', file: 'songs/kar_har_maidan_fateh.mp3', cover: 'covers/cover.jpeg' },
+  { name: 'Kar Har Maidaan Fateh', file: 'songs/kar_har_maidaan_fateh.mp3', cover: 'covers/cover.jpeg' },
   {name:'bandana',file:'songs/bandana.mp3',cover: 'covers/cover.jpeg' }
 ];
 export default songs;
